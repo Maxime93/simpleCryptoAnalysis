523ccec4f963848d38984e3413ad126512ebb3ba
maxime.richard <maxime.richard@moonshotlabs.com>
2019-07-06 21:32:06 -0700

    work

--- macd/Jenkinsfile.groovy
+++ macd/Jenkinsfile.groovy
@@ -2,40 +2,39 @@ node {
     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'psql-db', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
         sh("""echo uname=$USERNAME pwd=$PASSWORD""")
     }
-}
-
-pipeline {
-    agent {
-        docker {
-            image 'jupyter/datascience-notebook'
-            args '--user root:root'
+    pipeline {
+        agent {
+            docker {
+                image 'jupyter/datascience-notebook'
+                args '--user root:root'
+                }
+        }
+        // agent any
+        stages {
+            stage('Install..') {
+                steps {
+                    script {
+                        sh("""ls -la
+                        python --version
+                        pip install -r macd/requirements.txt
+                        echo env.USERNAME
+                        echo env.PASSWORD
+                        """)
+                    }
+                }
             }
-    }
-    // agent any
-    stages {
-        stage('Install..') {
-            steps {
-                script {
-                    sh("""ls -la
-                    python --version
-                    pip install -r macd/requirements.txt
-                    echo env.USERNAME
-                    echo env.PASSWORD
-                    """)
+            stage('RUN') {
+                steps {
+                    script {
+                        sh("""python3 macd/runner.py -u $USERNAME -p $PASSWORD""")
+                    }
                 }
             }
         }
-        stage('RUN') {
-            steps {
-                script {
-                    sh("""python3 macd/runner.py -u env.USERNAME -p env.PASSWORD""")
-                }
+        post {
+            always {
+            step([$class: 'WsCleanup']) /* clean up our workspace */
             }
-        }
-    }
-    post {
-        always {
-           step([$class: 'WsCleanup']) /* clean up our workspace */
         }
     }
 }
