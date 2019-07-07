dcaa256bcfdbeb401ce27cc9193633192288b58a
maxime.richard <maxime.richard@moonshotlabs.com>
2019-07-06 21:33:28 -0700

    work

--- macd/Jenkinsfile.groovy
+++ macd/Jenkinsfile.groovy
@@ -1,40 +1,45 @@
-node {
-    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'psql-db', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-        sh("""echo uname=$USERNAME pwd=$PASSWORD""")
+// node {
+//     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'psql-db', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+//         sh("""echo uname=$USERNAME pwd=$PASSWORD""")
+//     }
+// }
+pipeline {
+    agent {
+        docker {
+            image 'jupyter/datascience-notebook'
+            args '--user root:root'
+            }
     }
-    pipeline {
-        agent {
-            docker {
-                image 'jupyter/datascience-notebook'
-                args '--user root:root'
-                }
-        }
-        // agent any
-        stages {
-            stage('Install..') {
-                steps {
-                    script {
-                        sh("""ls -la
-                        python --version
-                        pip install -r macd/requirements.txt
-                        echo env.USERNAME
-                        echo env.PASSWORD
-                        """)
-                    }
-                }
+    // agent any
+    stages {
+        environment {
+            withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'psql-db', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
+                sh("""echo uname=$USERNAME pwd=$PASSWORD""")
             }
-            stage('RUN') {
-                steps {
-                    script {
-                        sh("""python3 macd/runner.py -u $USERNAME -p $PASSWORD""")
-                    }
+        }
+        stage('Install..') {
+            steps {
+                script {
+                    sh("""ls -la
+                    python --version
+                    pip install -r macd/requirements.txt
+                    echo env.USERNAME
+                    echo env.PASSWORD
+                    """)
                 }
             }
         }
-        post {
-            always {
-            step([$class: 'WsCleanup']) /* clean up our workspace */
+        stage('RUN') {
+            steps {
+                script {
+                    sh("""python3 macd/runner.py -u $USERNAME -p $PASSWORD""")
+                }
             }
+        }
+    }
+    post {
+        always {
+        step([$class: 'WsCleanup']) /* clean up our workspace */
         }
     }
 }
