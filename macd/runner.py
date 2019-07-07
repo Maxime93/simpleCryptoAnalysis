980f50097aabf4afb18bc1d63b135efe27b710a7
maxime.richard <maxime.richard@moonshotlabs.com>
2019-07-07 15:37:20 -0700

    finished macd script

--- macd/runner.py
+++ macd/runner.py
@@ -1,12 +1,11 @@
 import requests
+import sys
 import pandas as pd
 import datetime
 import argparse
 import stockstats
+import psycopg2
 from sqlalchemy import create_engine
-
-def get_filename(from_symbol, to_symbol, exchange, datetime_interval, download_date):
-    return '%s_%s_%s_%s_%s.csv' % (from_symbol, to_symbol, exchange, datetime_interval, download_date)
 
 def download_data(from_symbol, to_symbol, exchange, datetime_interval):
     supported_intervals = {'minute', 'hour', 'day'}
@@ -36,9 +35,18 @@ def filter_empty_datapoints(df):
     df = df.drop(indices)
     return df
 
-def savePostgres(uname, pwd, host, port, db):
+def queryPostgres(sql, conn):
+    cur = conn.cursor()
+    cur.execute(sql)
+    dates = cur.fetchall()
+    d = []
+    for i in dates:
+        d.append(i[0])
+    return d
+
+def savePostgres(df, table, uname, pwd, host, port, db):
     engine = create_engine('postgresql://{user}:{pwd}@{host}:{port}/{db}'.format(user=uname, pwd=pwd, host=host, port=port, db=db))
-    df.to_sql('macd', engine, if_exists='append', index=False)
+    df.to_sql(table, engine, if_exists='append', index=False)
 
 if __name__ == '__main__':
     # Arguments
@@ -51,23 +59,44 @@ if __name__ == '__main__':
     pwd = args.password
 
     # Download data
-    data = download_data('BTC', 'USD', 'coinbase', 'day')
+    # ###### PARAMS ######
+    from_symbol, to_symbol = 'BTC', 'USD'
+    exchange = 'coinbase'
+    period = 'day' # day / hour / minute
+    table = 'macd_{from_symbol}_{to_symbol}_{exchange}_{period}'.format(from_symbol=from_symbol.lower(), to_symbol=to_symbol.lower(), exchange=exchange, period=period)
+    # ###### ######
+
+    data = download_data(from_symbol, to_symbol, exchange, period)
     df = convert_to_dataframe(data)
     df = filter_empty_datapoints(df)
     current_datetime = datetime.datetime.now().date().isoformat()
-    filename = get_filename('BTC', 'USD', 'coinbase', 'day', current_datetime)
-    # print('Saving data to %s' % filename)
-    # df.to_csv("data/{}".format(filename), index=False)
 
     # MACD
     df = stockstats.StockDataFrame.retype(df)
     df['macd'] = df.get('macd')
-    print(df)
 
-    # Save DataFrame
+    # PSQL 
     host = '51.38.38.106'
     db = 'crypto'
     port = 5432
-    savePostgres(uname, pwd, host, port, db)
+
+    # Get days
+    sql = "SELECT DISTINCT datetime FROM {table};".format(table=table)
+    conn = psycopg2.connect(host=host, user=uname, password=pwd, dbname=db, port=port)
+    dates = queryPostgres(sql, conn)
+    conn.close()
+
+    # New DataFrame with new data
+    df = df[~df['datetime'].isin(dates)]
+    rows = len(df)
+    print("{} new rows to insert in DB.".format(rows))
+
+    # Save DataFrame
+    if rows == 0:
+        print("Not inserting anything in DF. 0 new rows.")
+    elif rows > 0:
+        print("Saving {} new rows..".format(rows))
+        savePostgres(df, table, uname, pwd, host, port, db)
+        print("Saved {} new rows!".format(rows))
 
     
